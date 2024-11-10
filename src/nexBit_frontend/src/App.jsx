2cf3b74b561bd05e8034df9f5aa11b8c588c0579
Noah Sheldon <noahsheldon06@gmail.com>
2024-11-10 14:53:12 +0000

    Block Headers Component

--- src/nexBit_frontend/src/App.jsx
+++ src/nexBit_frontend/src/App.jsx
@@ -1,21 +1,21 @@
+// App.js
 import React from 'react';
 import Navbar from './shared/Navbar';
 import BalanceComponent from './components/BalanceComponent';
-import FeePercentileComponent from './components/FeePercentileComponent';
 import UtxoComponent from './components/UtxoComponent';
-// import TransactionComponent from './components/TransactionComponent';
+import FeePercentileComponent from './components/FeePercentileComponent';
+import BlockHeaderComponent from './components/BlockHeaderComponent';
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-gray-900 to-gray-800 text-white font-sans">
       <Navbar />
       <main className="p-6 md:p-12">
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <BalanceComponent />
-          <FeePercentileComponent />
           <UtxoComponent />
-          {/* Uncomment the following line to include the TransactionComponent */}
-          {/* <TransactionComponent /> */}
+          <FeePercentileComponent />
+          <BlockHeaderComponent />
         </div>
       </main>
     </div>
