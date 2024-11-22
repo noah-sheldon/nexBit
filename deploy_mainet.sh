c690a6cf81904b37589572833cbae9d0166b6423
Noah Sheldon <noahsheldon06@gmail.com>
2024-11-22 11:50:37 +0000

    folder restructure

--- deploy_mainet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export BITCOIN_NODE="ic:bitcoin-mainnet"
-dfx ping ic
-dfx deploy nexBit_frontend --network ic --argument '(variant { mainnet })'
--- panic_icp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kill -9 $(lsof -t -i:4943) 2>/dev/null && echo "Process killed" || echo "No process found"
